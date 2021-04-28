40825e816dfb15637e26d45c53816199858c9d2f
abduallahB <64614781+abduallahB@users.noreply.github.com>
2021-04-28 16:11:57 +0300

    abduallah-abdulrahman-js-array  (#96)
    * challeng-2
    
    * challenge-1
    
    * update
    
    * class 2 challenge
    
    * challenge 3
    
    * update fiels name
    
    * update
    
    * update abduallah-belal file name
    
    * dom-array-challenge
    
    * class5
    
    * update dir name of class5
    
    * update dir name of class6
    
    * last update

---
+++ class-5-js-array/abduallah-abdulrahman/test.js
@@ -0,0 +1,20 @@
+// var convertString = function (str) {
+//     var s = '' ;
+//     var i = 0 ;
+//     while (i < str.length) {
+//         var n = str.charAt(i);
+//         if (n == n.toUpperCase()) {
+//             // *Call* toLowerCase
+//             n = n.toLowerCase();
+//         } else {
+//             // *Call* toUpperCase
+//             n = n.toUpperCase();
+//         }
+
+//         i += 1;
+//         s += n; 
+//     }
+//     return s;
+// };
+
+//  convertString("lower UPPER");
