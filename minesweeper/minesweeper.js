369d781ba194679fe97bbd54e9d0f9b653080138
Ammar-64 <ammar.n.sammour@gmail.com>
2021-05-26 01:12:28 +0300

    formatted minesweeper

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -1,38 +1,37 @@
 const sweeper = (grid) => {
-    const newGrid = [];
-    for (let i = 0; i < grid.length; i++) {
-      const row = [];
-      for (let j = 0; j < grid[i].length; j++) {
+  const newGrid = [];
+  for (let i = 0; i < grid.length; i++) {
+    const row = [];
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === 1) {
+        row.push(9);
+      } else {
+        row.push(neighbourChecker(grid, i, j));
+      }
+    }
+    newGrid.push(row);
+  }
+  return newGrid;
+};
+
+const neighbourChecker = (grid, rowIndex, colIndex) => {
+  let counter = 0;
+  for (let i = rowIndex - 1; i <= rowIndex + 1 && i < grid.length; i++) {
+    if (!!grid[i]) {
+      for (let j = colIndex - 1; j <= colIndex + 1 && j < grid[i].length; j++) {
         if (grid[i][j] === 1) {
-          row.push(9);
-        } else {
-          row.push(neighbourChecker(grid, i, j));
-        }
-      }
-      newGrid.push(row);
-    }
-    return newGrid;
-  };
-  
-  const neighbourChecker = (grid, rowIndex, colIndex) => {
-    let counter = 0;
-    for (let i = rowIndex - 1; i <= rowIndex + 1 && i < grid.length; i++) {
-      if (!!grid[i]) {
-        for (let j = colIndex - 1; j <= colIndex + 1 && j < grid[i].length; j++) {
-          if (grid[i][j] === 1) {
-            ++counter;
-          }
+          ++counter;
         }
       }
     }
-    console.log(counter);
-    return counter;
-  };
-  
-  sweeper( [
-  
-      [0, 1, 0, 0],
-      [0, 0, 1, 0],
-      [0, 1, 0, 1],
-      [1, 1, 0, 0],
-    ])
+  }
+  console.log(counter);
+  return counter;
+};
+
+sweeper([
+  [0, 1, 0, 0],
+  [0, 0, 1, 0],
+  [0, 1, 0, 1],
+  [1, 1, 0, 0],
+]);
